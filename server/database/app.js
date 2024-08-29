fa1c949a48afbda4676ef535931afe108d86f06d
vizvasrj <vizvasrj@gmail.com>
2024-08-29 13:54:56 -0400

    module 3 complete with node express deleter review fetch list details

--- server/database/app.js
+++ server/database/app.js
@@ -31,6 +31,7 @@ try {
 }
 
 
+
 // Express route to home
 app.get('/', async (req, res) => {
     res.send("Welcome to the Mongoose API")
@@ -59,16 +60,34 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+    try {
+        const documents = await Dealerships.find();
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+try {
+    const documents = await Dealerships.find({state: req.params.state});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+try {
+    const documents = await Dealerships.find({id: req.params.id});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 //Express route to insert review
