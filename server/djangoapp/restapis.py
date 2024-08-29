d8eafdc9e6f1e09ab7fa1b007723b5aaef34cfb7
vizvasrj <vizvasrj@gmail.com>
2024-08-29 15:30:30 -0400

    kubectl added

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -2,6 +2,7 @@
 # import requests
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
@@ -34,7 +35,7 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except KeyError:
         # If any error occurs
         print("Network exception occurred")
 
